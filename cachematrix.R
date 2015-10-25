eb95343efd402d62b960d4b39931267cfb607a81
Mark Fischer <fischm67@gmail.com>
2015-10-25 08:20:28 -0600

    updated code with comments for final submisison

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## The makeCacheMatrix and cacheSolve functions are included below
 
-## Write a short comment describing this function
+## makeCacheMatrix will create an object in the global environment which allows cacheSolve to work on creating the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL                                           ## instantiate a local m variable to allow cacheSolve to assign
+    set <- function(y) {                                ## define function set, overrides original matrix and resets global m variable
+        if(!is.matrix(y)) stop("Must be a matrix")      ## check to make sure you are assigning a matrix data type
+        x <<- y                                         ## if it is a matrix then use superassignment to right over original
+        m <<- NULL                                      ## superassign cached global variable so cacheSolve will re-compute the inverse
+    }
+    get <- function() x                                 ## returns the current matrix
+    setInverse <- function(setImatrix) {                ## untilized by cacheMatrix to store inverse matrix in global variable m
+        if(!is.matrix(setImatrix)) stop("Must be a matrix")
+        m <<- setImatrix                                ## use superassignment to store inverse matrix
+    }
+    getInverse <- function() m                          ## returns inverse matrix
+    list(set = set, get = get,                          ## list out available functions with the makeCacheMatrix object
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Assuming current matrix has an inverse cacheSolve will compute
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()                                 ## utilize getInverse function from makeCacheMatrix object
+    if(!is.null(m)) {                                   ## if global m variable has been reset to NULL cacheSolve will compute inverse
+        message("getting cached data")                  ## output message indicating the cached inverse matrix is being returned
+        return(m)
+    }
+    data <- x$get()                                     ## if global m variable is NULL utilize get function from makeCacheMatrix to get current matrix
+    m <- solve(data, ...)                               ## assign local m variable to the inverse matrix
+    x$setInverse(m)                                     ## utilize setInverse to set the global m variable to latest inverse matrix
+    m                                                   ## return inverse matrix to user
 }
